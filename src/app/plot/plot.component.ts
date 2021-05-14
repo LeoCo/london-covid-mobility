e50759d6beb27046a4327d54e4781899e209f8a1
Leonardo Cotti <leonardo.cotti@gmail.com>
2021-05-14 21:49:54 +0100

    Removed chart on event

--- src/app/plot/plot.component.ts
+++ src/app/plot/plot.component.ts
@@ -68,18 +68,6 @@ export class PlotComponent implements OnInit {
     this.filterForm.valueChanges.subscribe(value => {this.onUpdate()})
   }
 
-  onSelect(data: any): void {
-    console.log('Item clicked', JSON.parse(JSON.stringify(data)))
-  }
-
-  onActivate(data: any): void {
-    console.log('Activate', JSON.parse(JSON.stringify(data)))
-  }
-
-  onDeactivate(data: any): void {
-    console.log('Deactivate', JSON.parse(JSON.stringify(data)))
-  }
-
   onUpdate() {
     console.log('Update!')
     const metric = this.filterForm.value.metric
